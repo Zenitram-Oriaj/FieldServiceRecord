9dc09e22fb59bff650eb4fd125faa1e8900fd8fc
Jairo Martinez <jairo@digime.net>
2017-01-29 21:32:06 -0800

    Additional Files

---
+++ FieldServiceRecord/Pages/HistoryPageCS.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace FieldServiceRecord
+{
+  public class HistoryPageCS : ContentPage
+  {
+    public HistoryPageCS ()
+    {
+      Title = "History";
+      Padding = new Thickness (0, Device.OnPlatform (20, 0, 0), 0, 0);
+
+      Content = new StackLayout {
+        Children = {
+          new Label { Text = "History" }
+        }
+      };
+    }
+  }
+}
+
---
+++ iOS/FileHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+using FieldServiceRecord.iOS;
+
+[assembly: Dependency (typeof (FileHelper))]
+namespace FieldServiceRecord.iOS
+{
+  public class FileHelper : IFileHelper
+  {
+    public string GetLocalFilePath (string filename)
+    {
+      string docFolder = Environment.GetFolderPath (Environment.SpecialFolder.Personal);
+      string libFolder = Path.Combine (docFolder, "..", "Library", "Databases");
+
+      if (!Directory.Exists (libFolder)) {
+        Directory.CreateDirectory (libFolder);
+      }
+
+      return Path.Combine (libFolder, filename);
+    }
+  }
+}
